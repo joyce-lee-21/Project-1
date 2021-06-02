cb098eaf4e51273f56539ec29d9d341171e517e8
Joyce Lee <joycelee901@gmail.com>
2021-06-02 16:07:41 -0500

    updated with fetch to db.json

--- index.js
+++ index.js
@@ -1,36 +1,56 @@
-const init = () => {
+// const init = () => {
+//     getAddresses
+//     // setUpListeners()
+// }
+
+document.addEventListener("DOMContentLoaded", () => {
     getAddresses()
-    setUpListeners()
-}
+    // setUpListeners()
+})
+
 //need to fetch from db.json file rather than outside link
-const updateAddress = (desc) => {
-    fetch('url',{
-      //  method: "GET"
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ description: desc })
+//fetched addresses from db.json needs to create list of addresses
+const getAddresses = () => {
+    fetch('http://localhost:3000/addresses')
+    .then(res => res.json())
+    .then(data => data.forEach(getAddressInfo))
+    .catch(error => console.log("Error: ", error))
+}
+
+//“As a user, I want click on a home from the list so that the information (address, picture, name) shows up.”
+
+const getAddressInfo = (data) => {
+    console.log(data)
+
+    [listPLACEHOLDER].addEventListener('click', () => {
+
     })
 }
 
-const setUpListeners = () => {
-    const ...form = document.querySelector('...')
-    ...FormData.addEventListner('click,submit,scroll')
-}
+// const selectAddress = (desc) => {
+//     fetch('url',{
+//       //  method: "GET"
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({ description: desc })
+//     })
+        // const addressName = document.querySelector('h2')
+        // addressName.innerText = data.name
 
-const getAddresses = () => {
-    fetch("url")
-    .then(res => res.json())
-    .then(console.log(addresses))
-}
+        // const addressImg = document.querySelector('img')
+        // addressImg.src = data.image
 
-const getAddressInfo = () => {
-    const addressName = document.querySelector('h')
-    addressName.innerText = address.name
+        // const addressDesc = document.querySelector('h3')
+        // addressDesc.innerText = data.address
+// }
 
-    const addressImg = document.querySelector('img')
-    addressImg.src = address.image_url
+// const setUpListeners = () => {
+//     const ...form = document.querySelector('...')
+//     ...FormData.addEventListner('click,submit,scroll')
+// }
 
-    const addressDesc = document.querySelector('description').querySelector('textarea')
-    addressDesc.innerText = address.description
-}
+// “As a user, I want click this "map it" button so that the embedded map locates the home.”
+
+
+// “As a user, I want input my current location so that I can get a route to the selected home.”
