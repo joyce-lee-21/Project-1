ed70911e3cc606ac11d2e5fd58b76d29881f3f4a
Joyce Lee <joycelee901@gmail.com>
2021-06-03 13:56:06 -0500

    added generated google map

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-// const init = () => {
-//     getAddresses
-//     // setUpListeners()
-// }
+const API_KEY = "AIzaSyCyKMXUad_LOaxvAirtrIayiSYALdq2x6I"
 
 document.addEventListener("DOMContentLoaded", () => {
     getAddresses()
-    // setUpListeners()
 })
 
 //need to fetch from db.json file rather than outside link
@@ -21,9 +17,10 @@ const getAddresses = () => {
 
 const getAddressInfo = (data) => {
     // console.log(data)
-
     let li = document.createElement('li')
     li.innerText = data.name
+    li.style.color = "#fffff4"
+    li.style.color = "#fffff4"
 
     document.querySelector('ul').append(li)
 
@@ -33,15 +30,7 @@ const getAddressInfo = (data) => {
 }
 
 const selectAddress = (e, data) => {
-    console.log(data)
-    // fetch('url',{
-
-    //   //  method: "GET"
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify({ description: desc })
-    // })
+    // console.log(data)
     const mapArea = document.querySelector('div#map-content')
 
     const addressName = document.querySelector('h2.home-name')
@@ -54,27 +43,59 @@ const selectAddress = (e, data) => {
     addressDesc.innerText = ""
     addressDesc.innerText = data.address
 
-
     const addressImg = document.querySelector('img.home-img')
     // addressImg.className = "home-img"
     addressImg.src = ""
     addressImg.src = data.image
 
-    mapArea.append(addressName, addressDesc, addressImg)
+    const addressMapIt = document.createElement('button')
+    addressMapIt.className = "google-map"
+    addressMapIt.innerText = "Map It!"
+
+    mapArea.append(addressName, addressDesc, addressImg, addressMapIt)
+
+    // “As a user, I want click this "map it" button so that the embedded map locates the home.”
+    addressMapIt.addEventListener('click', () => {
+        console.log(data.address)
+
+        const destination = data.address
+        const embeddedMap = document.createElement('div')
+        embeddedMap.className = "embedded-map"
+
+        const addressMap = document.createElement('iframe')
+
+        addressMap.className = "home-map"
+        addressMap.width= "500"
+        addressMap.height="350"
+        addressMap.frameborder="0" 
+        addressMap.style = "border: 0; padding-top: 50px;"
+        addressMap.allowfullscreen = true
+        addressMap.src = `https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${destination}`
+        // `https://www.google.com/maps/embed/v1/directions?key=${API_KEY}&origin=${origin}&destination=${destination}`
 
 
+        mapArea.append(embeddedMap)
+        embeddedMap.append(addressMap)
+
+        let addressForm = document.createElement('form')
+        let addressInput = document.createElement('input')
+        let addressSubmit = document.createElement('input')
+
+        addressForm.className = 'address-form'
+        addressInput.className = 'address-input'
+        addressInput.placeholder = "Starting Point"
+        addressSubmit.className = 'address-submit'
+        addressSubmit.setAttribute("type", "button")
+        addressSubmit.value = "Get Directions"
+
+        embeddedMap.append(addressForm)
+        addressForm.append(addressInput, addressSubmit)
+
+        // “As a user, I want input my current location so that I can get a route to the selected home.”
+        addressSubmit.addEventListener('click', () => {
+        })
+    })
 }
 
-const placeMap = () => {
-
-}
-
-// const setUpListeners = () => {
-//     const ...form = document.querySelector('...')
-//     ...FormData.addEventListner('click,submit,scroll')
-// }
-
-// “As a user, I want click this "map it" button so that the embedded map locates the home.”
 
 
-// “As a user, I want input my current location so that I can get a route to the selected home.”
